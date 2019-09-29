c53688d078ea5a5693a18e989607fd211c500fec
hyyking <leoduret@outlook.com>
2019-09-29 16:48:17 +0200

    feat(rsc2-pb): TryIntoEnum impl

--- rsc2-pb/build.rs
+++ rsc2-pb/build.rs
@@ -1,11 +1,15 @@
 extern crate prost_build;
 
+const DERIVE_WRAPENUM: &str = "#[derive(::rsc2_derive::WrapEnum)]";
+const DERIVE_TRYINTOENUM: &str = "#[derive(::rsc2_derive::TryIntoEnum)]";
+
 fn main() {
     let mut prost_build = prost_build::Config::new();
     prost_build.btree_map(&["."]);
 
     // SC2 impl Into<request::Request> for Request.request types
-    prost_build.type_attribute("Request.request", "#[derive(::rsc2_derive::WrapEnum)]");
+    prost_build.type_attribute("Request.request", DERIVE_WRAPENUM);
+    prost_build.type_attribute("Status", DERIVE_TRYINTOENUM);
 
     prost_build
         .compile_protos(
