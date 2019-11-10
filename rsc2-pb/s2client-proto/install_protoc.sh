ece3d19e3c571ae9e6569b4cf5a14f391b477032
hyyking <leoduret@outlook.com>
2019-11-10 13:05:37 +0100

    refactor(*): cleanup repository

--- rsc2-pb/s2client-proto/install_protoc.sh
+++
@@ -1,5 +0,0 @@
-wget https://github.com/google/protobuf/releases/download/v3.4.0/protoc-3.4.0-linux-x86_64.zip
-unzip protoc-3.4.0-linux-x86_64.zip -d protoc3 && \
-mv protoc3/bin/* /usr/bin/ && \
-mv protoc3/include/* /usr/include/ && \
-rm -rf protoc*
